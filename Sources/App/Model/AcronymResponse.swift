c0a721017258d66907108baa7296553f1a7887a3
karun-pantexport <karunpant@gmail.com>
2022-12-30 11:40:40 +0530

    Create and retrieve operations added.

---
+++ Sources/App/Model/AcronymResponse.swift
@@ -0,0 +1,30 @@
+//
+//  AcronymResponse.swift
+//  
+//
+//  Created by Karun Pant on 30/12/22.
+//
+
+import Vapor
+
+struct AcronymResponse: Content {
+    let errorDescription: String?
+    let accronyms: [AcronymItem]
+    init(errorDescription: String? = nil,
+         accronyms: [AcronymItem] = []) {
+        self.errorDescription = errorDescription
+        self.accronyms = accronyms
+    }
+}
+
+struct AcronymItem: Content {
+    let short: String
+    let long: String
+    let displayText: String
+    
+    init(acronym: Acronym) {
+        short = acronym.short
+        long = acronym.long
+        displayText = "'\(acronym.short)' stands for '\(acronym.long)'"
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,18 +2,38 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async in
-        "It works!"
-    }
-
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
-    
+    // [C]reate
     app.post("api", "v1", "acronym", "add") { req -> EventLoopFuture<Acronym> in
         let acronym = try req.content.decode(Acronym.self)
         return acronym.save(on: req.db).map {
             acronym
         }
+    }
+    // [R]etrieve all
+    app.get("api", "v1", "acronyms") { req -> EventLoopFuture<AcronymResponse> in
+        Acronym.query(on: req.db).all().map({ acronyms in
+            var acroymItems: [AcronymItem] = []
+            for acronym in acronyms {
+                acroymItems.append(AcronymItem(acronym: acronym))
+            }
+            return AcronymResponse(accronyms: acroymItems)
+        })
+    }
+    // [R]etrieve single/specific
+    app.get("api", "v1", "acronym", ":short") { req -> EventLoopFuture<AcronymResponse> in
+        let shortName = req.parameters.get("short") ?? ""
+        return Acronym.query(on: req.db)
+            .filter(\.$short == shortName)
+            .sort(\.$short)
+            .all().map({ acronyms in
+                guard !acronyms.isEmpty else {
+                    return AcronymResponse(errorDescription: "Cannot find any acronym for '\(shortName)', You may want to send name all uppercased and all lowercased.")
+                }
+                var acroymItems: [AcronymItem] = []
+                for acronym in acronyms {
+                    acroymItems.append(AcronymItem(acronym: acronym))
+                }
+                return AcronymResponse(accronyms: acroymItems)
+            })
     }
 }
