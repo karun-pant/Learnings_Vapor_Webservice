00d97121a42314a99b63423f0f755a594f1d70c8
karun-pantexport <karunpant@gmail.com>
2022-12-31 13:20:44 +0530

    CRUD Operations complete.

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,6 +9,14 @@ func routes(_ app: Application) throws {
             acronym
         }
     }
+    
+    // [R]etrieve all as Acronym
+    app.get("api", "v1", "acronym_base") { req -> [Acronym] in
+        try await Acronym.query(on: req.db).all().map { acronyms in
+            acronyms
+        }
+    }
+    
     // [R]etrieve all
     app.get("api", "v1", "acronyms") { req -> EventLoopFuture<AcronymResponse> in
         Acronym.query(on: req.db).all().map({ acronyms in
@@ -27,7 +35,7 @@ func routes(_ app: Application) throws {
             .sort(\.$short)
             .all().map({ acronyms in
                 guard !acronyms.isEmpty else {
-                    return AcronymResponse(errorDescription: "Cannot find any acronym for '\(shortName)', You may want to send name all uppercased and all lowercased.")
+                    return AcronymResponse(errorDescription: "Cannot find any acronym for '\(shortName)', You may want to send name all uppercased or all lowercased.")
                 }
                 var acroymItems: [AcronymItem] = []
                 for acronym in acronyms {
@@ -36,4 +44,30 @@ func routes(_ app: Application) throws {
                 return AcronymResponse(accronyms: acroymItems)
             })
     }
+    
+    // [U]pdate find by id since by name you might get multiple items.
+    app.put("api", "v1", "acronym", ":id") { req -> EventLoopFuture<Acronym> in
+        let updatedAcronym = try req.content.decode(Acronym.self)
+        return Acronym.find(req.parameters.get("id"), on: req.db)
+            .unwrap(or: Abort(.badRequest))
+            .flatMap { acronym in
+                acronym.short = updatedAcronym.short
+                acronym.long = updatedAcronym.long
+                return acronym.save(on: req.db).map {
+                    acronym
+                }
+            }
+    }
+    
+    // [D]elete by finding id
+    app.delete("api", "v1", "acronym", ":id") { req -> EventLoopFuture<String> in
+        Acronym.find(req.parameters.get("id"),
+                     on: req.db)
+        .unwrap(or: Abort(.badRequest))
+        .flatMap { acronym in
+            acronym.delete(on: req.db)
+                .transform(to: "Data has been deleted successfully.")
+        }
+    }
+    
 }
